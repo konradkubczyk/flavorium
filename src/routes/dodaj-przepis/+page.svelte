4ecf31f60e9ed062de08b9eceac462f4b4dfd9e4
Konrad Kubczyk <konrad@kubczyk.com>
2023-11-17 01:04:25 +0100

    fix: add missing required attribute

--- src/routes/dodaj-przepis/+page.svelte
+++ src/routes/dodaj-przepis/+page.svelte
@@ -31,11 +31,12 @@
 				rows="3"
 				placeholder="Pyszne tosty, które zrobiłem wczoraj..."
 				bind:value={recipeData.description}
+				required
 			></textarea>
 		</div>
 		<div>
 			<label for="typeSelect">Wybierz Rodzaj</label>
-			<select id="typeSelect" bind:value={recipeData.type}>
+			<select id="typeSelect" bind:value={recipeData.type} required>
 				<option disabled selected value="">Wybierz </option>
 				<option value="food">jedzenie</option>
 				<option value="drink">napój</option>
@@ -43,20 +44,19 @@
 		</div>
 		<div>
 			<label for="subtype">Wybierz</label>
-			<select id="subtype" bind:value={recipeData.subType}>
+			<select id="subtype" bind:value={recipeData.subType} required>
 				{#if recipeData.type == 'drink'}
 					<option disabled selected value="">Wybierz typ napoju</option>
-
 					<option value="hotDrink">Ciepły napój</option>
 					<option value="coldDrink">Napój</option>
 					<option value="alcoholicDrink">Napój z alkoholem</option>
 				{:else if recipeData.type == 'food'}
 					<option disabled selected value="">Wybierz typ potrawy</option>
-					<option value="pasta">makaron</option>
-					<option value="soup">zupa</option>
-					<option value="pastery">pieczywo</option>
+					<option value="pasta">Makaron</option>
+					<option value="soup">Zupa</option>
+					<option value="pastery">Pieczywo</option>
 				{:else}
-					<option disabled selected value="">Wybierz Rodzaj powyżej </option>
+					<option disabled selected value="">Wybierz rodzaj powyżej</option>
 				{/if}
 			</select>
 		</div>
@@ -64,48 +64,6 @@
 			<input id="isVegan" type="checkbox" bind:checked={recipeData.isVegan} />
 			<label for="isVegan">Czy przepis jest wegański?</label>
 		</div>
-		<!-- <div class="form-group">
-			<label for="nip">NIP</label>
-			<input class="form-control" id="nip" required placeholder="NIP" />
-		</div>
-		<div class="form-group">
-			<label for="city">Miasto</label>
-			<input class="form-control" id="city" required placeholder="enter your email" />
-		</div>
-		<div class="form-group">
-			<label for="street">Ulica</label>
-			<input id="street" required placeholder="enter your email" />
-		</div>
-		<div class="form-group">
-			<label for="house-number">Numer domu </label>
-			<input id="house-number" required placeholder="enter your email" />
-		</div>
-		<div class="form-group">
-			<label for="flat-number">Numer mieszkania </label>
-			<input id="flat-number" placeholder="enter your email" />
-		</div>
-		<div class="form-group">
-			<label for="postal-code">Kod pocztowy</label>
-			<input id="postal-code" required placeholder="enter your email" />
-		</div>
-		<div class="form-group">
-			<label for="exampleFormControlTextarea1">Uwagi</label>
-			<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" cols="1"></textarea>
-		</div>
-		<div class="form-group">
-			<label for="exampleFormControlSelect2">Wybierz swoja branze z poniższej listy</label>
-			<select id="industry" class="custom-select my-1 mr-sm-2">
-				<option value="1">Budowlanka</option>
-				<option value="2">Konserwacja powierzchni płaskich</option>
-				<option value="3">Usługi</option>
-				<option value="4">Beuty</option>
-				<option value="5">Edukacja</option>
-			</select>
-		</div>
-		<div class="custom-control custom-checkbox my-1 mr-sm-2">
-			<input id="active" type="checkbox" class="custom-control-input" />
-			<label class="custom-control-label" for="customControlInline">Aktywny</label>
-		</div> -->
-		<button type="submit"> Dodaj przepis </button>
+		<button type="submit" on:submit={handleSubmit}>Dodaj przepis</button>
 	</form>
 </div>
