57135d68c2717462180b4c24cc70cf67b6d7196e
Konrad Kubczyk <konrad@kubczyk.com>
2023-12-17 01:59:36 +0100

    chore: (very) minor tweak

--- src/routes/przepisy/+page.ts
+++ src/routes/przepisy/+page.ts
@@ -1,7 +1,7 @@
 export async function load() {
 
     const res = await fetch(
-        `https://cloud.appwrite.io/v1/databases/655bd832ea6f472b15d8/collections/655bd852b4a5ec1fd409/documents`,
+        'https://cloud.appwrite.io/v1/databases/655bd832ea6f472b15d8/collections/655bd852b4a5ec1fd409/documents',
         {
             headers: {
                 'X-Appwrite-Project': '6556209062be5c6ce4b0',
