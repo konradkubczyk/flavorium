41a9afea6f13092199118e0b631b406f31b95017
Konrad Kubczyk <konrad@kubczyk.com>
2023-12-26 22:51:53 +0100

    feat: add persistent user account state

--- src/routes/konto/+page.svelte
+++ src/routes/konto/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { account } from '$lib/stores/account';
+	import * as accountService from '$lib/services/account'
 
 	interface Credentials {
 		name: string;
@@ -15,120 +17,50 @@
 	let processing = false;
 	let accountCreationMode = false;
 	let accountCreatedSuccessfully = false;
-	let isLoggedIn: boolean | null = null;
 
-	interface AuthError {
-		errorOccurred: boolean;
-		message: string;
-	}
-
-	let authError: AuthError = {
-		errorOccurred: false,
-		message: ''
-	};
+	let authError: string = '';
 
 	async function login() {
 		accountCreatedSuccessfully = false;
 		processing = true;
-		authError.errorOccurred = false;
-		const res = await fetch(
-			'https://cloud.appwrite.io/v1/account/sessions/email',
-			{
-				method: 'POST',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-					'X-Appwrite-Project': '6556209062be5c6ce4b0'
-				},
-				body: JSON.stringify({
-					email: credentials.email,
-					password: credentials.password
-				})
-			}
-		);
+		authError = '';
+		const status = await accountService.login(credentials.email, credentials.password);
 		processing = false;
-		if (res.status === 201) {
-			await res.json();
-			await checkIfLoggedIn();
-		} else if (res.status === 401) {
-			authError.errorOccurred = true;
-			authError.message = 'Nieprawidłowy email lub hasło.';
-		} else if (res.status === 429) {
-			authError.errorOccurred = true;
-			authError.message = 'Zbyt wiele prób logowania. Spróbuj ponownie później.';
-		} else {
-			authError.errorOccurred = true;
-			authError.message = 'Wystąpił błąd. Upewnij się, że wybrane hasło jest bezpieczne.';
+		switch (status) {
+			case 201:
+				break;
+			case 401:
+				authError = 'Nieprawidłowy email lub hasło.';
+				break;
+			case 429:
+				authError = 'Zbyt wiele prób logowania. Spróbuj ponownie później.'
+				break;
+			default:
+				authError = 'Wystąpił nieznany błąd.'
 		}
 	}
 
 	async function register() {
 		accountCreatedSuccessfully = false;
 		processing = true;
-		authError.errorOccurred = false;
-		const res = await fetch(
-			'https://cloud.appwrite.io/v1/account',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					'X-Appwrite-Project': '6556209062be5c6ce4b0'
-				},
-				body: JSON.stringify({
-					userId: 'unique()',
-					email: credentials.email,
-					password: credentials.password,
-					name: credentials.name
-				})
-			}
-		);
+		authError = '';
+		const status = await accountService.register(credentials.email, credentials.password, credentials.name);
 		processing = false;
-		if (res.status === 201) {
-			accountCreatedSuccessfully = true;
-			accountCreationMode = false;
-		} else if (res.status === 429) {
-			authError.errorOccurred = true;
-			authError.message = 'Zbyt wiele prób rejestracji. Spróbuj ponownie później.';
-		} else if (res.status === 409) {
-			authError.errorOccurred = true;
-			authError.message = 'Konto o podanym adresie email już istnieje.';
-		} else {
-			authError.errorOccurred = true;
-			authError.message = 'Wystąpił błąd. Sprawdź dane logowania i spróbuj ponownie.';
+		switch (status) {
+			case 201:
+				accountCreatedSuccessfully = true;
+				accountCreationMode = false;
+				break;
+			case 409:
+				authError = 'Konto o podanym adresie email już istnieje.';
+				break;
+			case 429:
+				authError = 'Wystąpił błąd. Sprawdź dane logowania i spróbuj ponownie.'
+				break;
+			default:
+				authError = 'Wystąpił błąd. Upewnij się, że wybrane hasło jest bezpieczne.'
 		}
 	}
-
-	async function logout() {
-		await fetch(
-			'https://cloud.appwrite.io/v1/account/sessions/current',
-			{
-				method: 'DELETE',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-					'X-Appwrite-Project': '6556209062be5c6ce4b0'
-				}
-			}
-		);
-		await checkIfLoggedIn();
-	}
-
-	async function checkIfLoggedIn() {
-		const res = await fetch(
-			'https://cloud.appwrite.io/v1/account/sessions/current',
-			{
-				method: 'GET',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-					'X-Appwrite-Project': '6556209062be5c6ce4b0'
-				}
-			}
-		);
-		isLoggedIn = res.status === 200;
-	}
-
-	checkIfLoggedIn();
 </script>
 
 <svelte:head>
@@ -136,21 +68,15 @@
 	<meta name="description" content="Strona konta użytkownika" />
 </svelte:head>
 
-{#if isLoggedIn === null }
-
-	<section class="container mx-auto py-80 text-center">
-		<div class="text-3xl animate-pulse text-cyan-800">Pobieranie danych, proszę czekać...</div>
-	</section>
-
-{:else if isLoggedIn === true }
+{#if $account.isLoggedIn }
 
 	<section class="container mx-auto py-80 text-center">
 		<div class="text-3xl text-cyan-800">Zalogowano. Chcesz się wylogować?</div>
-		<button on:click={logout} class="mt-5 py-2 px-3 rounded-lg bg-cyan-700 text-white hover:bg-cyan-600 active:scale-95 transition"
+		<button on:click={accountService.logout} class="mt-5 py-2 px-3 rounded-lg bg-cyan-700 text-white hover:bg-cyan-600 active:hover:bg-cyan-700 transition"
 		>Wyloguj</button>
 	</section>
 
-{:else if isLoggedIn === false }
+{:else }
 
 	<section class="container mx-auto text-amber-950">
 		<div class="flex flex-col-reverse xl:flex-row rounded-3xl overflow-hidden">
@@ -181,7 +107,7 @@
 									 class="p-2 rounded-lg focus:outline outline-amber-300 transition-all" disabled={processing}>
 						<div class="flex gap-5">
 							<button type="submit"
-											class="flex-1 p-2 rounded-lg bg-amber-300 hover:bg-amber-400 disabled:bg-amber-200 disabled:opacity-75 active:scale-95 transition"
+											class="flex-1 p-2 rounded-lg bg-amber-300 hover:bg-amber-400 disabled:bg-amber-200 disabled:opacity-75 active:bg-amber-500/80 transition"
 											disabled={processing}>
 								{#if processing}
 									Rejestracja...
@@ -190,7 +116,7 @@
 								{/if}
 							</button>
 							<button on:click={() => { accountCreationMode = false }}
-											class="flex-1 p-2 rounded-lg bg-white/75 hover:bg-amber-200/75 disabled:bg-amber-200/75 disabled:opacity-75 active:scale-95 transition"
+											class="flex-1 p-2 rounded-lg bg-white/75 hover:bg-amber-200/75 disabled:bg-amber-200/75 disabled:opacity-75 active:bg-amber-300/60 transition"
 											disabled={processing}>
 								Mam już konto
 							</button>
@@ -204,7 +130,7 @@
 									 class="p-2 rounded-lg focus:outline outline-amber-300 transition-all" disabled={processing}>
 						<div class="flex gap-5">
 							<button type="submit"
-											class="flex-1 p-2 rounded-lg bg-amber-300 hover:bg-amber-400 disabled:bg-amber-200 disabled:opacity-75 active:scale-95 transition"
+											class="flex-1 p-2 rounded-lg bg-amber-300 hover:bg-amber-400 disabled:bg-amber-200 disabled:opacity-75 active:bg-amber-500/80 transition"
 											disabled={processing}>
 								{#if processing}
 									Logowanie...
@@ -213,15 +139,15 @@
 								{/if}
 							</button>
 							<button on:click={() => { accountCreationMode = true }}
-											class="flex-1 p-2 rounded-lg bg-white/75 hover:bg-amber-200/75 disabled:bg-amber-200/75 disabled:opacity-75 active:scale-95 transition"
+											class="flex-1 p-2 rounded-lg bg-white/75 hover:bg-amber-200/75 disabled:bg-amber-200/75 disabled:opacity-75 active:bg-amber-300/60 transition"
 											disabled={processing}>
 								Nie mam konta
 							</button>
 						</div>
 					</form>
 				{/if}
-				{#if authError.errorOccurred}
-					<p class="mt-5 text-red-500 bg-rose-200 rounded-lg p-5">{authError.message}</p>
+				{#if authError}
+					<p class="mt-5 text-red-500 bg-rose-200 rounded-lg p-5">{authError}</p>
 				{/if}
 			</div>
 			<div class="w-full h-96 xl:h-auto xl:w-3/5 bg-cover bg-center"
