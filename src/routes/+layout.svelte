f706e7adc40e19be458dcfdc21f4b934e582c25e
Konrad Kubczyk <konrad@kubczyk.com>
2023-12-28 16:53:47 +0100

    feat: switch to SPA mode by disabling SSR

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 	import Footer from './Footer.svelte';
 	import './styles.css';
 	import "../app.css";
+
+	// Switch to SPA mode by disabling SSR
+	export const ssr = false;
 </script>
 
 <div class="app flex-1 flex flex-col justify-between">
